08f37ff4e6ff2883057920e13ba40d27ed73d4a3
Veer J Kamdar <veerpersonal1797@gmail.com>
2024-02-19 09:13:56 +0530

    qwerty

--- App.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-import React from "react";
-import AuthNav from "./src/navigation/AuthNav";
-
-const App = () => {
-  return (<AuthNav />);
-};
-
-export default App;
